369fc3aabd9554fb4a0e85a00f27a808ac98dc0e
shinyoung99 <shinyoung5906@gmail.com>
2024-03-26 09:36:33 +0900

    회원가입 페이지 수정 1

--- flutter_application_1/lib/SignUpPage.dart
+++ flutter_application_1/lib/SignUpPage.dart
@@ -13,23 +13,6 @@ class _SignUpPageState extends State<SignUpPage> {
   final TextEditingController _nicknameController = TextEditingController();
   String _errorMessage = '';
 
-  Future<void> _signUp() async {
-    try {
-      UserCredential userCredential = await _auth.createUserWithEmailAndPassword(
-        email: _emailController.text,
-        password: _passwordController.text,
-      );
-
-      // You can store additional user data like nickname in Firebase Firestore or Realtime Database.
-      // For now, let's just print the user's email.
-      print('Signed up: ${userCredential.user!.email}');
-    } catch (e) {
-      setState(() {
-        _errorMessage = e.toString();
-      });
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -80,5 +63,61 @@ class _SignUpPageState extends State<SignUpPage> {
         ),
       ),
     );
+  }
+
+  Future<void> _signUp() async {
+    try {
+      String email = _emailController.text.trim();
+      String password = _passwordController.text.trim();
+
+      if (email.isEmpty || password.isEmpty) {
+        // 이메일 또는 비밀번호가 비어있는 경우 처리
+        return;
+      }
+
+      bool isExist = await isEmailAlreadyExist(email);
+      if (isExist) {
+        // 이미 존재하는 이메일인 경우 처리
+        return;
+      }
+
+      UserCredential userCredential = await signUpWithEmailPassword(email, password);
+      // 회원가입 성공 시, 추가적인 작업 수행
+      print('Signed up: ${userCredential.user!.email}');
+    } catch (e) {
+      // 회원가입 실패 시, 에러 메시지 출력
+      setState(() {
+        _errorMessage = e.toString();
+      });
+    }
+  }
+
+  Future<bool> isEmailAlreadyExist(String email) async {
+    try {
+      UserCredential userCredential = await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: 'dummyPassword', // 임시 비밀번호
+      );
+      // 회원가입 성공 시, 생성된 사용자 정보를 삭제하여 중복 검사를 위해 회원가입 시도를 취소
+      await userCredential.user?.delete();
+      return false; // 중복되지 않은 이메일
+    } catch (e) {
+      if (e is FirebaseAuthException && e.code == 'email-already-in-use') {
+        return true; // 중복된 이메일
+      } else {
+        throw e; // 그 외의 오류는 예외 처리
+      }
+    }
+  }
+
+  Future<UserCredential> signUpWithEmailPassword(String email, String password) async {
+    try {
+      return await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+    } catch (e) {
+      throw e;
+    }
   }
 }
