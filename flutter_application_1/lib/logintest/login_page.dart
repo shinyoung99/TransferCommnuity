4b280f62b17a131c284cc56d89315678f57cc49f
shinyoung99 <shinyoung5906@gmail.com>
2024-04-16 19:44:05 +0900

    vscode 마지막

--- flutter_application_1/lib/logintest/login_page.dart
+++ flutter_application_1/lib/logintest/login_page.dart
@@ -1,4 +1,3 @@
-
 // ignore_for_file: use_build_context_synchronously
 
 import 'package:firebase_auth/firebase_auth.dart';
@@ -6,22 +5,21 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:my_flutter_app/main.dart';
 import 'package:my_flutter_app/logintest/my_home_page.dart';
- 
+
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
- 
+
   @override
   State<LoginPage> createState() => _LoginPageState();
 }
- 
+
 class _LoginPageState extends State<LoginPage> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-  final TextEditingController _emailController = TextEditingController(); //입력되는 값을 제어
+  final TextEditingController _emailController =
+      TextEditingController(); //입력되는 값을 제어
   final TextEditingController _passwordController = TextEditingController();
 
-
- 
-  Widget _userIdWidget(){
+  Widget _userIdWidget() {
     return TextFormField(
       controller: _emailController,
       keyboardType: TextInputType.emailAddress,
@@ -29,16 +27,17 @@ class _LoginPageState extends State<LoginPage> {
         border: OutlineInputBorder(),
         labelText: '이메일',
       ),
-      validator: (String? value){
-        if (value!.isEmpty) {// == null or isEmpty
+      validator: (String? value) {
+        if (value!.isEmpty) {
+          // == null or isEmpty
           return '이메일을 입력해주세요.';
         }
         return null;
       },
     );
   }
- 
-  Widget _passwordWidget(){
+
+  Widget _passwordWidget() {
     return TextFormField(
       controller: _passwordController,
       obscureText: true,
@@ -47,15 +46,16 @@ class _LoginPageState extends State<LoginPage> {
         border: OutlineInputBorder(),
         labelText: '비밀번호',
       ),
-      validator: (String? value){
-        if (value!.isEmpty) {// == null or isEmpty
+      validator: (String? value) {
+        if (value!.isEmpty) {
+          // == null or isEmpty
           return '비밀번호를 입력해주세요.';
         }
         return null;
       },
     );
   }
- 
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -79,14 +79,12 @@ class _LoginPageState extends State<LoginPage> {
                 width: double.infinity,
                 padding: const EdgeInsets.only(top: 8.0), // 8단위 배수가 보기 좋음
                 child: ElevatedButton(
-                    onPressed: () => _login(),
-                    child: const Text("로그인")
-                ),
+                    onPressed: () => _login(), child: const Text("로그인")),
               ),
               const SizedBox(height: 20.0),
               GestureDetector(
                 child: const Text('회원 가입'),
-                onTap: (){
+                onTap: () {
                   Get.to(() => const JoinPage());
                 },
               ),
@@ -96,12 +94,13 @@ class _LoginPageState extends State<LoginPage> {
       ),
     );
   }
- 
+
   @override
   void initState() {
     //해당 클래스가 호출되었을떄
     super.initState();
   }
+
   @override
   void dispose() {
     // 해당 클래스가 사라질떄
@@ -109,24 +108,24 @@ class _LoginPageState extends State<LoginPage> {
     _passwordController.dispose();
     super.dispose();
   }
- 
+
   _login() async {
     //키보드 숨기기
     if (_formKey.currentState!.validate()) {
       FocusScope.of(context).requestFocus(FocusNode());
- 
+
       // Firebase 사용자 인증, 사용자 등록
       try {
         await FirebaseAuth.instance.signInWithEmailAndPassword(
           email: _emailController.text,
           password: _passwordController.text,
         );
- 
+
         Get.offAll(() => const MarketPage());
       } on FirebaseAuthException catch (e) {
         logger.e(e);
         String message = '';
- 
+
         if (e.code == 'user-not-found') {
           message = '사용자가 존재하지 않습니다.';
         } else if (e.code == 'wrong-password') {
@@ -134,14 +133,14 @@ class _LoginPageState extends State<LoginPage> {
         } else if (e.code == 'invalid-email') {
           message = '이메일을 확인하세요.';
         }
- 
+
         /*final snackBar = SnackBar(
           content: Text(message),
           backgroundColor: Colors.deepOrange,
       );
       ScaffoldMessenger.of(context).showSnackBar(snackBar);
       */
- 
+
         ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(
             content: Text(message),
@@ -149,10 +148,8 @@ class _LoginPageState extends State<LoginPage> {
           ),
         );
       }
- 
     }
   }
- 
 }
 
 class JoinPage {
